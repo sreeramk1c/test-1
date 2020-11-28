150e79202174262e77d811cf38d4f0d64d022d32
Sreeram K <sreeramfireball@gmail.com>
2020-11-28 18:58:21 +0530

    basics restored

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <html>
 	<body>
 		<h1> TEST </h1>
-		<?php
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "testdb1";
-		
-			$conn = new mysqli($servername, $username, $password, $dbname); 
-			if ($conn->connect_error) {
-				echo "err";
-				$conn->close();
-			}
-			$sql = "select * from chess";
-			$result = $conn->query($sql);
-			if($result->num_rows > 0) {
-				while($row = $result->fetch_assoc()) {
-					echo "fide id:".$row['fideid']."-"."Name:".$row['name'];
-				}
-			}
-			else {
-				echo "No rows";
-			}
-			$conn->close();	
-		?>
+		<h1>SLOO SREENIDHI</h1>
 	</body>
 </html>
